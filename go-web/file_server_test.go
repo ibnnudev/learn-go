102a307cb587e9c1e30d8a2a2ef45d0bdc175f98
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2025-05-02 11:17:39 +0700

    Enable GitHub Copilot for all file types and add file server tests with embedded resources

--- go-web/file_server_test.go
+++ go-web/file_server_test.go
@@ -1,6 +1,8 @@
 package goweb
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 	"testing"
 )
@@ -13,6 +15,27 @@ func TestFileServer(t *testing.T) {
 
 	mux.Handle("/resources/", http.StripPrefix("/resources/", fs))
 
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Fatalf("Server failed to start: %v", err)
+	}
+}
+
+//go:embed resources
+var resources embed.FS
+
+func TestEmbedFile(t *testing.T) {
+	subFS, _ := fs.Sub(resources, "resources")
+	directory := http.FileServer(http.FS(subFS))
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/resources/", http.StripPrefix("/resources/", directory))
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
