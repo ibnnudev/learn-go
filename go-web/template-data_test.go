9b7ad736a031e79cf4c38eff933646c00c29d358
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2025-05-02 13:07:08 +0700

    Add TemplateData function and simple.gohtml template for rendering HTML

---
+++ go-web/template-data_test.go
@@ -0,0 +1,28 @@
+package goweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateData(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/simple.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", map[string]interface{}{
+		"title": "Simple Website",
+		"name":  "Ibnnu Dev",
+	})
+}
+
+func TestTemplateData(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateData(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
