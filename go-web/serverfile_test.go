6b0f75192b7b1f95e50956254262a6d809e82f5e
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2025-05-02 11:26:03 +0700

    Add ServeFileEmbed function and corresponding test for serving embedded file

--- go-web/serverfile_test.go
+++ go-web/serverfile_test.go
@@ -1,6 +1,8 @@
 package goweb
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -17,6 +19,30 @@ func TestServeFile(t *testing.T) {
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Fatalf("Failed to start server: %v", err)
+	}
+	defer server.Close()
+}
+
+//go:embed resources/ok.txt
+var okFile string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("file") != "" && r.URL.Query().Get("file") == "ok" {
+		fmt.Fprint(w, okFile)
+	} else {
+		http.Error(w, "File not found", http.StatusNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
 	}
 
 	err := server.ListenAndServe()
