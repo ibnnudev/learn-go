a465479b96215f82dcf6a1242924f05f42a43569
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2025-05-04 12:44:35 +0700

    - method not allowed - not found

--- go-web/go-http-router/panic_handle_test.go
+++ go-web/go-http-router/panic_handle_test.go
@@ -31,3 +31,46 @@ func TestPanicHandler(t *testing.T) {
 
 	assert.Equal(t, "Panic occurred:This is a panic", string(body))
 }
+
+func TestNotFoundHandler(t *testing.T) {
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Page not found")
+	})
+
+	request := httptest.NewRequest("GET", "/nonexistent", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	assert.Equal(t, "Page not found", string(body))
+}
+
+func TestMethodNotAllowedHandler(t *testing.T) {
+	router := httprouter.New()
+
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Method not allowed")
+		fmt.Println("Method not allowed")
+		fmt.Println("Request method:", r.Method)
+		fmt.Println("Request URL:", r.URL)
+	})
+
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello, HTTP Router!")
+	})
+
+	request := httptest.NewRequest("POST", "/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	assert.Equal(t, "Method not allowed", string(body))
+}
